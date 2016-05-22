985806911533019a151f51aa3e361f488ee277e7
Jamie Graziano <jamiefgraziano@msn.com>
2016-05-22 13:24:43 -0700

    Adding Shrimp Farm Application

---
+++ ShrimpFarm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShrimpFarm.MvcApplication" Language="C#" %>
---
+++ ShrimpFarm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShrimpFarm.Startup))]
+namespace ShrimpFarm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
